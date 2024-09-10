83a5123c9613c21d39b10795f75f4336db4fd6f2
tjran7017 <tjran7017@gmail.com>
2024-09-10 13:40:48 +0900

    fix: font family

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
-import React, { lazy, Suspense } from 'react';
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { Layout, Loading, Resume } from './comp';
-
-const Index = lazy(() =>
-  new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(null);
-    }, 1500);
-  }).then(() => import('./comp/Resume/ResumeContainer')),
-);
+import { Layout, Resume } from './comp';
 
 function App() {
   return (
     <Layout>
-      <Suspense fallback={<Loading type="page" />}>
-        <Routes>
-          <Route path="/" element={<Index />} />
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route path="/" element={<Resume />} />
+      </Routes>
     </Layout>
   );
 }
