ba3e6c0026ea19acfc2b6772ee8352a976e9af25
tjran7017 <tjran7017@gmail.com>
2024-09-10 12:05:02 +0900

    feat: add css load logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { Layout, Loading, Resume } from './comp';
+import { Layout, Resume } from './comp';
 
 function App() {
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import '../public/assets/css/index.scss';
 import App from './App';
+
+const loadCSS = (href: string): Promise<void> =>
+  new Promise((resolve, reject) => {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = href;
+    link.onload = () => resolve();
+    link.onerror = () => reject(new Error('CSS load error'));
+
+    document.head.appendChild(link);
+  });
+
+const Root: React.FC = () => {
+  const [cssLoaded, setCssLoaded] = useState(false);
+
+  useEffect(() => {
+    loadCSS('/assets/css/style.css').then(() => {
+      setCssLoaded(true);
+    });
+  }, []);
+
+  if (!cssLoaded) {
+    return null;
+  }
+
+  return <App />;
+};
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
 );
